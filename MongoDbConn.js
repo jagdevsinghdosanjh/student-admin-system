931ba47259b533feaa18e0977fd3b13b675c166c
jagdevsinghdosanjh <jagdevsinghdosanjh@gmail.com>
2025-02-21 20:30:26 +0530

    Ok

--- MongoDbConn.js
+++ MongoDbConn.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const dbURI = 'mongodb://localhost:27017/studAdminDb';
+const dbURI = 'mongodb://localhost:27017/AdminStudentDb';
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
 .then(() => console.log('Connected to MongoDB'))
 .catch(err => console.error(err));
--- app.js
+++ app.js
@@ -1,3 +1,28 @@
+// const express = require('express');
+// const mongoose = require('mongoose');
+// const bodyParser = require('body-parser');
+// const cors = require('cors');
+// const authRoutes = require('./routes/auth');
+// const quizRoutes = require('./routes/quiz');
+// const studentRoutes = require('./routes/student');
+
+// const app = express();
+
+// // Middleware
+// app.use(bodyParser.json());
+// app.use(cors());
+
+// // Routes
+// app.use('/api/auth', authRoutes);
+// app.use('/api/quizzes', quizRoutes);
+// app.use('/api/students', studentRoutes);
+
+// // Database Connection
+// const dbURI = 'mongodb://localhost:27017/AdminStudentDb';
+// mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+// .then(() => app.listen(5000, () => console.log('Server running on port 5000')))
+// .catch(err => console.error(err));
+
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
@@ -18,7 +43,6 @@ app.use('/api/quizzes', quizRoutes);
 app.use('/api/students', studentRoutes);
 
 // Database Connection
-const dbURI = 'mongodb://localhost:27017/';
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-.then(() => app.listen(5000, () => console.log('Server running on port 5000')))
-.catch(err => console.error(err));
+mongoose.connect('mongodb://localhost:27017/AdminStudentDb', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(5000, () => console.log('Server running on port 5000')))
+  .catch(err => console.error(err));
